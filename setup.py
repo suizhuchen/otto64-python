e2aa7c656f2fdc2ba0122bce206e001f3c742a52
Suichen <lyfdacallmain@163.com>
2025-01-22 18:16:47 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='otto64',
     version='0.0.1',
     description='otto64 encoding/decoding',
-
+    url='https://github.com/suizhuchen/otto64-python',
+    author='suizhuchen & hua-zhi-wan',
 )
